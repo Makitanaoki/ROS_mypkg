5fb344705506327741d65e64c5147b4b8a0b8383
Naoki Makita <a@a>
2021-01-07 10:02:53 +0000

    2

--- scripts/three.py
+++ scripts/three.py
@@ -3,8 +3,8 @@
 import rospy
 from std_msgs.msg import Int32
 
-rospy.init_node('count')
-pub = rospy.Publisher('count_up', Int32, queue_size=1)
+rospy.init_node('three')
+pub = rospy.Publisher('three', Int32, queue_size=1)
 rate = rospy.Rate(2)
 n = 0
 while not rospy.is_shutdown():
--- scripts/twice.py
+++ scripts/twice.py
@@ -3,15 +3,22 @@ import rospy
 from std_msgs.msg import Int32
 
 n = 0
+x = 0
 
 def cb(message):
     global n
     n = message.data*2
 
+def db(message2):
+    global x
+    x = message2.data*(-2)
+
 rospy.init_node('twice')
 sub = rospy.Subscriber('count_up', Int32, cb)
+sub = rospy.Subscriber('three', Int32, db)
 pub = rospy.Publisher('twice', Int32, queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(2)
 while not rospy.is_shutdown():
     pub.publish(n)
+    pub.publish(x)
     rate.sleep()

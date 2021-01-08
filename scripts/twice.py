e7722c2eedf1060bea4000d0216f22811843bfff
Makitanaoki <s17C1123AP@s.chibakoudai.jp>
2021-01-08 15:47:50 +0900

    Update twice.py

--- scripts/twice.py
+++ scripts/twice.py
@@ -14,13 +14,13 @@ def cb(message):
 
 def db(message2):
     global x
-    x = message2.data*(-2)
+    x = message2.data*2
 
 rospy.init_node('twice')
 sub = rospy.Subscriber('count_up', Int32, cb)
 sub = rospy.Subscriber('three', Int32, db)
 pub = rospy.Publisher('twice', Int32, queue_size=1)
-rate = rospy.Rate(2)
+rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     pub.publish(n)
     pub.publish(x)

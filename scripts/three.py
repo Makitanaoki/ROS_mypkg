0d707708de3acc807e684a5187a0fc3b84527ea0
Makitanaoki <s17C1123AP@s.chibakoudai.jp>
2021-01-08 15:47:14 +0900

    Update three.py

--- scripts/three.py
+++ scripts/three.py
@@ -7,9 +7,9 @@ from std_msgs.msg import Int32
 
 rospy.init_node('three')
 pub = rospy.Publisher('three', Int32, queue_size=1)
-rate = rospy.Rate(2)
+rate = rospy.Rate(1)
 n = 0
 while not rospy.is_shutdown():
-    n += 1
+    n -= 1
     pub.publish(n)
     rate.sleep()
